d684925868dee303108383d96e7ad3c90bc802d3
Chaitanya Arora <127611582+chaitanyaa7@users.noreply.github.com>
2024-03-12 10:59:45 +0530

    made the page responsive

--- src/components/home/features.tsx
+++ src/components/home/features.tsx
@@ -1,55 +1,50 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-  
-  export function Features() {
-    return (
-        <div className="flex gap-4 max-w-[600px]">
-        <Card>
+export function Features() {
+  return (
+    <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mx-5 sm:mx-0  gap-6  max-w-[600px]">
+      <Card>
         <CardHeader>
           <CardTitle>Anonymous</CardTitle>
         </CardHeader>
         <CardContent>
-          <p>Onyx offers a safe space for open discussions without real-world identies</p>
+          <p>
+            Onyx offers a safe space for open discussions without real-world
+            identies
+          </p>
         </CardContent>
       </Card>
 
-
-<Card>
+      <Card>
         <CardHeader>
           <CardTitle>Community</CardTitle>
         </CardHeader>
         <CardContent>
-          <p>Join a dynamic community of college students sharing ideas and connecting</p>
+          <p>
+            Join a dynamic community of college students sharing ideas and
+            connecting
+          </p>
         </CardContent>
       </Card>
 
-
-
-
-<Card>
+      <Card>
         <CardHeader>
           <CardTitle>Discussions</CardTitle>
         </CardHeader>
         <CardContent>
-          <p>Onyx offers a safe space for open discussions without real-world identies</p>
+          <p>
+            Onyx offers a safe space for open discussions without real-world
+            identies
+          </p>
         </CardContent>
       </Card>
-        </div>
-        
-
-
-
-    )
-  }
-
-  
-
-
- 
+    </div>
+  );
+}
--- src/components/nav/HomeNav.tsx
+++ src/components/nav/HomeNav.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { Button } from '../ui/button';
+import React from "react";
+import { Button } from "../ui/button";
 
 const HomeNav = () => {
   return (
-    <nav className="flex px-16 justify-between py-5 font-['Montserrat']">
-      <div>
-        <h4 className="text-4xl">onyx</h4>
-      </div>
+    <nav className="flex px-4 items-center md:px-16  justify-between py-5 font-['Montserrat']">
+      <h4 className="text-4xl">onyx</h4>
 
-      <div className="flex gap-10 mr-10 md:mr-20">
+      <div className="flex gap-10 md:mr-20">
         <Button className="text-xl font-semibold" variant={"link"}>
           about
         </Button>
@@ -18,6 +16,6 @@ const HomeNav = () => {
       </div>
     </nav>
   );
-}
+};
 
-export default HomeNav
+export default HomeNav;
