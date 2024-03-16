308dc349c281d1ca3f351be583960e331f0af9ba
Chirayu31 <chirayupancholi17@gmail.com>
2024-03-17 00:22:08 +0530

    added trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { SiGoogle } from "react-icons/si";
-import { Features } from "@/components/home/features";
-import HomeNav from "@/components/nav/HomeNav";
+'use client'
+import { Button } from '@/components/ui/button'
+import { SiGoogle } from 'react-icons/si'
+import { Features } from '@/components/home/features'
+import HomeNav from '@/components/nav/HomeNav'
+import { trpc } from '@/utils/trpc'
 
 export default function Home() {
+  const hello = trpc.hello.useQuery({ text: 'Chirayu' })
+
+  if (hello.data) {
+    console.log(hello.data.greeting)
+  }
+
   return (
     <>
       <HomeNav />
-      <main className="flex w-full flex-col items-center">
+      <main className='flex w-full flex-col items-center'>
         <h2 className="text-center scroll-m-20  border-b py-10 pb-2 text-5xl font-semibold tracking-wide first:mt-0 font-['Sedgwick_Ave_Display']">
           PSEUDONYMOUS FORUM FOR IPS STUDENTS
         </h2>
@@ -16,14 +24,14 @@ export default function Home() {
           UNVEIL YOUR THOUGHTS, CONNECT WITH PEERS, STAY UNSEEN
         </blockquote>
 
-        <Button className="text-xl font-semibold mt-10 h-12 ">
-          <SiGoogle className="mr-2 h-3 w-3 text-centre " /> Login with Google
+        <Button className='text-xl font-semibold mt-10 h-12 '>
+          <SiGoogle className='mr-2 h-3 w-3 text-centre ' /> Login with Google
         </Button>
 
-        <div className="flex w-full justify-center mt-10 ">
+        <div className='flex w-full justify-center mt-10 '>
           <Features />
         </div>
       </main>
     </>
-  );
+  )
 }
