7996b43eada47627ef217df1cfbef8eaa4f080c0
Chirayu31 <chirayupancholi17@gmail.com>
2024-03-20 22:27:31 +0530

    added parentCommentId field for reply comments

---
+++ prisma/migrations/20240320153957_unique_email_reply_comment_id/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "Comment" ADD COLUMN     "parentCommentId" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_parentCommentId_fkey" FOREIGN KEY ("parentCommentId") REFERENCES "Comment"("id") ON DELETE SET NULL ON UPDATE CASCADE;
